95fc02355bcfbf252a3762a1b9f58e8aa1393de8
Will Adams <bushblade@gmail.com>
2019-07-08 20:23:21 +0100

    added react spring

--- src/components/Routes/SharedResults.js
+++ src/components/Routes/SharedResults.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Card, Col, Row, Button } from 'react-bootstrap'
+import { useTrail, animated, config } from 'react-spring'
+
 import ModalTrailer from '../Result/ModalTrailer'
 
 const imageBaseUrl = 'https://image.tmdb.org/t/p/original/'
@@ -7,6 +9,16 @@ const imageBaseUrl = 'https://image.tmdb.org/t/p/original/'
 const SharedResults = ({ url }) => {
   const [movies, setMovies] = useState([])
   const [modal, setModalId] = useState(null)
+
+  const trail = useTrail(movies.length, {
+    opacity: 1,
+    transform: 'translate3d(0,0,0)',
+    from: {
+      opacity: 0,
+      transform: 'translate3d(0,+50px,0)'
+    },
+    config: config.stiff
+  })
 
   useEffect(() => {
     fetch(url)
@@ -21,24 +33,27 @@ const SharedResults = ({ url }) => {
       .catch(console.log)
   }, [url])
 
-  const showMovies = movies.map(movie => (
-    <Col xs={12} md={4} lg={4} key={movie.id} className="my-4">
-      <Card className="my-3 h-100 shadow">
-        <Card.Img
-          variant="top"
-          src={`${imageBaseUrl}${movie.backdrop_path || movie.poster_path}`}
-        />
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <span className="text-center">
-            <Button onClick={e => setModalId(movie.id)} className="trailer-button">
-              Trailer
-            </Button>
-          </span>
-        </Card.Body>
-      </Card>
-    </Col>
-  ))
+  const showMovies = trail.map((props, index) => {
+    const movie = movies[index]
+    return (
+      <animated.div style={props} key={movie.id} className="my-4 col-lg-4 col-md-4 col-12">
+        <Card className="my-3 h-100 shadow">
+          <Card.Img
+            variant="top"
+            src={`${imageBaseUrl}${movie.backdrop_path || movie.poster_path}`}
+          />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <span className="text-center">
+              <Button onClick={e => setModalId(movie.id)} className="trailer-button">
+                Trailer
+              </Button>
+            </span>
+          </Card.Body>
+        </Card>
+      </animated.div>
+    )
+  })
 
   return (
     <Container>
