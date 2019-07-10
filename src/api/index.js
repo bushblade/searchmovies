a2c2df3d5d08b50f7d613f16d41bce86ee6b5421
Will Adams <bushblade@gmail.com>
2019-07-10 20:03:55 +0100

    custom hook useMovies

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,7 @@ export const nowPlaying = `${baseUrl}/movie/now_playing?${key}`
 export const discover = `${baseUrl}/discover/movie?${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 
 export const topRated = `${baseUrl}/movie/top_rated?${key}&language=en-US&page=1`
+
+export const upcomingMovies = `${baseUrl}/movie/upcoming?${key}&language=en-US&region=US&with_release_type=2|3`
+
+export const imageBaseUrl = 'https://image.tmdb.org/t/p/original/'
